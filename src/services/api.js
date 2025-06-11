1c8295959f10eecefd87428942853b641e597f2a
lL <118486423+efrathorvitz@users.noreply.github.com>
2025-06-11 10:20:26 +0300

    change to old api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const baseUrl = 'https://committed-delight-2680eb60f9.strapiapp.com';
-const baseUrl = 'https://attractive-canvas-7b6d0f80fe.strapiapp.com';
+const baseUrl = 'https://committed-delight-2680eb60f9.strapiapp.com';
+//const baseUrl = 'https://attractive-canvas-7b6d0f80fe.strapiapp.com';
 export const fetchEntities = async (collectionName) => {
   try {
     console.log("fetch  "+collectionName)
