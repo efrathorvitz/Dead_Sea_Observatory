7fca98e2fcff9a6bd4366bfaf7953360fb0d821a
lL <118486423+efrathorvitz@users.noreply.github.com>
2025-10-30 14:17:29 +0200

    change image gallery and photo in entity

--- src/utils/getFullImageUrl.js
+++ src/utils/getFullImageUrl.js
@@ -1,6 +1,6 @@
 // utils/getFullImageUrl.js
 export const getFullImageUrl = (image) => {
-  const baseURL = "https://committed-delight-2680eb60f9.strapiapp.com/";
+  const baseURL = "https://committed-delight-2680eb60f9.media.strapiapp.com/";
   if (!image) return null;
   return image.url || (image.hash ? `${baseURL}${image.hash}${image.ext}` : null);
 };
